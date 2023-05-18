034f68b0d08d294bec787fe15b57e6b24425212d
Billy Somers <somersbilly@gmail.com>
2023-05-18 13:17:05 +1000

    Fixed issue

--- components/layouts/News.tsx
+++ components/layouts/News.tsx
@@ -44,7 +44,7 @@ export default function News(){
                     </div>
                 </div>
                 <div className="block max-w-sm rounded bg-white shadow dark:bg-neutral-700">
-                        < href="#!">
+                        <Link href="#!">
                         <Image
                             className="rounded-t"
                             src="/"
